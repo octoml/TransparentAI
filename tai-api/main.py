0599b0bb99c4ee1ad480a84a1c0dbac705b75e57
Spencer Krum <nibz@spencerkrum.com>
2022-06-03 01:21:33 -0500

    Make tai api server accept json
    Before this anything with Content-Type: application/json caused a 422
    error

--- tai-api/main.py
+++ tai-api/main.py
@@ -2,11 +2,18 @@ from typing import Union, Optional, List
 
 from fastapi import FastAPI
 from .engine import get_engine
+from pydantic import BaseModel
+
+
+class Prompt(BaseModel):
+    prompt: str
 
 app = FastAPI()
 
+
+
 @app.post("/engines/{engine_name}/completions")
-def complete_with_engine(engine_name: str, prompt: Optional[Union[str, List[str]]] = None):
+def complete_with_engine(engine_name: str, prompt: Prompt):
     engine = get_engine(engine_name)
-    response = engine.complete(prompt)
-    return { "engine_name": engine_name, "prompt": prompt, "response": response }
+    response = engine.complete(prompt.prompt)
+    return { "engine_name": engine_name, "prompt": prompt.prompt, "response": response }
