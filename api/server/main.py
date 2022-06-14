4bac25567ee48dd4c050f60e999c828826832b24
Gaz Iqbal <giqbal@octoml.ai>
2022-06-14 22:57:52 +0000

    cleanup

--- api/server/main.py
+++ api/server/main.py
@@ -5,22 +5,15 @@ from fastapi import FastAPI, UploadFile
 from fastapi.responses import Response  # FileResponse, StreamingResponse
 from PIL import Image
 
+from model import MODEL_IMAGE_HEIGHT, MODEL_IMAGE_WIDTH, get_remote_model
 from utils.image import (
     image_crop_center,
     image_from_normalized_ndarray,
     image_to_normalized_ndarray,
 )
-from utils.triton import TritonRemoteModel
-
-MODEL_EXPECTED_WIDTH = 256
-MODEL_EXPECTED_HEIGHT = 256
 
 app = FastAPI()
-model = TritonRemoteModel(
-    "host.docker.internal:8000",
-    "magenta_arbitrary-image-stylization-v1-256_2.tar",
-    protocol="http",
-)
+model = get_remote_model()
 
 
 @app.post("/stylize/")
@@ -28,13 +21,13 @@ async def generate_image(source_image_file: UploadFile, style_image_file: Upload
     source_image = Image.open(source_image_file.file)
     print(source_image.format, source_image.size, source_image.mode)
     source_image_array = image_to_normalized_ndarray(
-        image_crop_center(source_image, MODEL_EXPECTED_WIDTH, MODEL_EXPECTED_HEIGHT)
+        image_crop_center(source_image, MODEL_IMAGE_WIDTH, MODEL_IMAGE_HEIGHT)
     )
 
     style_image = Image.open(style_image_file.file)
     print(style_image.format, style_image.size, style_image.mode)
     style_image_array = image_to_normalized_ndarray(
-        image_crop_center(style_image, MODEL_EXPECTED_WIDTH, MODEL_EXPECTED_HEIGHT)
+        image_crop_center(style_image, MODEL_IMAGE_WIDTH, MODEL_IMAGE_HEIGHT)
     )
 
     result = model(style_image_array, source_image_array)
--- api/server/test.py
+++ api/server/test.py
@@ -3,37 +3,30 @@ from os import path
 import numpy as np
 from PIL import Image
 
+from model import MODEL_IMAGE_HEIGHT, MODEL_IMAGE_WIDTH, get_remote_model
 from utils.image import (
     image_crop_center,
     image_from_normalized_ndarray,
     image_to_normalized_ndarray,
 )
-from utils.triton import TritonRemoteModel
-
-MODEL_EXPECTED_WIDTH = 256
-MODEL_EXPECTED_HEIGHT = 256
 
 image_base_path = path.abspath(path.join(path.dirname(__file__), "../tests/data/"))
 
 image_content_file = path.join(image_base_path, "Source_Golden_Gate.jpg")
 image_content_array = image_to_normalized_ndarray(
     image_crop_center(
-        Image.open(image_content_file), MODEL_EXPECTED_WIDTH, MODEL_EXPECTED_HEIGHT
+        Image.open(image_content_file), MODEL_IMAGE_WIDTH, MODEL_IMAGE_HEIGHT
     )
 )
 
 image_style_file = path.join(image_base_path, "Style_Kanagawa.jpg")
 image_style_array = image_to_normalized_ndarray(
     image_crop_center(
-        Image.open(image_style_file), MODEL_EXPECTED_WIDTH, MODEL_EXPECTED_HEIGHT
+        Image.open(image_style_file), MODEL_IMAGE_WIDTH, MODEL_IMAGE_HEIGHT
     )
 )
 
-model = TritonRemoteModel(
-    "host.docker.internal:8000",
-    "magenta_arbitrary-image-stylization-v1-256_2.tar",
-    protocol="http",
-)
+model = get_remote_model()
 result = model(image_style_array, image_content_array)
 result_image = image_from_normalized_ndarray(np.squeeze(result[0]))
 result_image.save("styled.jpg")
